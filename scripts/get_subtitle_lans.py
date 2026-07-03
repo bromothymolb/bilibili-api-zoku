92a204ebddcda67b1d70f1bb0291a3afb1c74772
Nemo2011 <yimoxia@outlook.com>
2026-07-03 19:43:40 +0800

    chore(data): update related data json files

--- scripts/get_subtitle_lans.py
+++ scripts/get_subtitle_lans.py
@@ -6,7 +6,7 @@ async def main() -> None:
     res = await Api(
         url="https://s1.hdslb.com/bfs/subtitle/subtitle_lan.json", method="GET"
     ).request(raw=True)
-    print(json.dumps(res, ensure_ascii=False))
+    print(json.dumps(res, ensure_ascii=False, indent=4))
 
 
 sync(main())
