a0839d025ad0d0719b8be92e58cb6d0df58a3ba3
bromothymolb <bromothymolb@qq.com>
2026-07-29 10:41:19 +0800

    fix: wbi

--- bilibili_api/article.py
+++ bilibili_api/article.py
@@ -568,7 +568,12 @@ class Article:
         """
 
         api = API["info"]["view"]
-        params = {"id": self.__cvid}
+        params = {
+            "id": self.__cvid,
+            "from": "web",
+            "gaia_source": "main_web",
+            "web_location": "333.976",
+        }
         return (
             await Api(**api, credential=self.credential).update_params(**params).result
         )
@@ -582,7 +587,12 @@ class Article:
         """
 
         api = API["info"]["detail"]
-        params = {"id": self.__cvid}
+        params = {
+            "id": self.__cvid,
+            "from": "web",
+            "gaia_source": "main_web",
+            "web_location": "333.976",
+        }
         return (
             await Api(**api, credential=self.credential).update_params(**params).result
         )
