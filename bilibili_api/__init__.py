5db1e388c5ebb8abc89ad55cff68f4a8cba6f8e6
bromothymolb <bromothymolb@qq.com>
2026-08-18 10:09:27 +0800

    chore: update project.license

--- bilibili_api/__init__.py
+++ bilibili_api/__init__.py
@@ -142,7 +142,7 @@ from .utils.picture import Picture
 from .utils.short import get_real_url
 from .utils.sync import sync
 
-BILIBILI_API_VERSION = "18.0.0.b0"
+BILIBILI_API_VERSION = "18.0.0.b0+0"
 
 
 import importlib
