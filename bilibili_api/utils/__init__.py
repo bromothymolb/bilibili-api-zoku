45be35d5684f2d52912be14ef3a1714a22de6a8d
bromothymolb <bromothymolb@qq.com>
2026-08-21 10:15:52 +0800

    chore: merge varint.py into BytesReader.py

---
+++ bilibili_api/utils/__init__.py
@@ -0,0 +1,27 @@
+"""
+bilibili_api.utils
+
+- AsyncEvent: 异步事件类
+- BytesReader: protobuf 字节数据解析
+- danmaku: 弹幕实例类
+- geetest: 极验验证实例类
+- picture: 图片类
+- upos: Upos 文件上传类
+
+- aid_bvid_transformer: aid, bvid 互换
+- initial_state: __INITIAL_STATE__ 支持
+- parse_link: 链接解析支持 (parse_link)
+- short: 短链解析支持 (get_real_url)
+- user_render_data: 用户动态页渲染数据支持
+
+- anti_spider: 反爬虫相关
+- high_level: Credetial / Api / bili_download
+- network: 会话调度支持
+
+- cache_pool: 模块数据缓存
+- logger: 日志支持
+- models: BiliAPIClient 和过滤器相关模型
+- settings: 设置支持
+- sync: 同步执行异步代码
+- utils: 工具类/工具函数
+"""
